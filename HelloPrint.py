0bf5d0b865574becfb0ce41153c3ed3552f0ce58
Usevich <tisiztomsk@gmail.com>
2024-02-21 20:26:11 +0700

    выложил

---
+++ HelloPrint.py
@@ -0,0 +1,5 @@
+def hello1():
+    print ('Hello, world')
+
+def hello2():
+    print ('Hello, world еще раз')
---
+++ main.py
@@ -0,0 +1,6 @@
+#Вызываем из модуля функции под именами h1 и h2
+from HelloPrint import hello1 as h1, hello2 as h2
+
+h1()
+h2()
+h1()
